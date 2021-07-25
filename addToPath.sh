dc616c5e82cb703089b1b4c9e2f68b04dc9d96e7
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2021-07-25 05:09:32 +0100

    chore: Added silent argument to 'addToPath' script

--- addToPath.sh
+++ addToPath.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 pathToCheckFor="$HOME/.local/bin"
+[[ $1 = "-s"  ]] || [[ $1 = "--silent" ]] && shouldBeSilent=false || shouldBeSilent=true
 
 echo $PATH | grep -q $pathToCheckFor
 
 if [ $? -eq 1 ]; then
-  echo "/.local/bin not found in PATH, adding..."
+  [ "$shouldBeSilent" = false ] && echo "/.local/bin not found in PATH, adding..."
   export PATH=$PATH:/home/olaolu/.local/bin
-  echo "/.local/bin added."
+  [ "$shouldBeSilent" = false ] && echo "/.local/bin added."
 fi
